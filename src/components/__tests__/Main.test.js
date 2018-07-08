cf110e0c25ce9d9e6b19827592f15d0638b4ff16
David Jimenez <david.jimenez19@gmail.com>
2018-07-08 19:15:44 +0200

    add jest support

---
+++ src/components/__tests__/Main.test.js
@@ -0,0 +1,8 @@
+const assert = require('assert');
+const Main = require('../Main.js');
+
+describe('Main', () => {
+  it('has a test', () => {
+    assert(false, 'Main should have a test');
+  });
+});
